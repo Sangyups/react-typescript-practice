d0a1e342d0d438fcb86748ac6dfceef49a181032
Sangyups <feels4637@gmail.com>
2022-08-31 11:24:42 +0900

    feat: add ExpenseItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
 }
 
 export default App;
